0c8f7e8978f025511334804a4a7f03d763eede6e
Shenghai Yuan <140951558+SHYuanBest@users.noreply.github.com>
2024-06-29 20:41:43 +0800

    Update run_eliminate_video.sh

--- Multi-Aspect_Preprocessing/video_redundancy_elimination/run_eliminate_video.sh
+++ Multi-Aspect_Preprocessing/video_redundancy_elimination/run_eliminate_video.sh
@@ -1,8 +1,8 @@
 # If you can't connect to huggingface.co, add this line of code (hf-mirror)
-export HF_ENDPOINT=https://hf-mirror.com
+# export HF_ENDPOINT=https://hf-mirror.com
 model_name="test"
 input_folder="../../toy_video"
 output_folder="classify_videos"
-model_pth='/remote-home1/ysh/0_ckpts/OpenGVLab/InternVideo2-Stage2_1B-224p-f4/InternVideo2-stage2_1b-224p-f4.pt'
+model_pth='InternVideo2-stage2_1b-224p-f4.pt'
 
 python video_redundancy_elimination.py --model_name $model_name --input_folder $input_folder --output_folder $output_folder --model_pth $model_pth
